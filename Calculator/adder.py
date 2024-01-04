b51f3d94ee3154ca1c320ecdce9c69e5dc01c0dd
ghoshaishik505 <47148051+ghoshaishik505@users.noreply.github.com>
2024-01-04 15:51:46 +0530

    Adding the library

---
+++ Calculator/adder.py
@@ -0,0 +1,15 @@
+# addition_library/addition_library/adder.py
+
+class Adder:
+    def add_numbers(self, num1, num2):
+        """
+        Add two numbers and return the result.
+
+        Parameters:
+        - num1 (float or int): The first number.
+        - num2 (float or int): The second number.
+
+        Returns:
+        - float or int: The sum of num1 and num2.
+        """
+        return num1 + num2
---
+++ setup.py
@@ -0,0 +1,12 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='addition-library',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies here if needed
+    ],
+)
