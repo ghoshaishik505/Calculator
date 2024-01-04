2625f413bac9399a69185380a05db7f1c91a41fa
ghoshaishik505 <47148051+ghoshaishik505@users.noreply.github.com>
2024-01-04 15:59:52 +0530

    Update setup.py with dependancies

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version='0.1',
     author = 'Aishik Ghosh',
     packages=find_packages(),
-    install_requires=[
+    install_requires=[ 'pandas==1.4.4'
         # Add any dependencies here if needed
     ],
 )
