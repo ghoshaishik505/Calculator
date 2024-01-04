d3ba9f10716aab23de5b44a202c340712adf525e
ghoshaishik505 <47148051+ghoshaishik505@users.noreply.github.com>
2024-01-04 15:54:22 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name='addition-library',
     version='0.1',
+    author = 'Aishik Ghosh',
     packages=find_packages(),
     install_requires=[
         # Add any dependencies here if needed
